35c4311c335e8314e400f507988d6a547a7f147a
Misael Lima <misaellimadossantos@hotmail.com>
2021-09-27 10:31:33 -0400

    Implementando o metodo incluirEquipe e alterarEquipe na EquipeController

--- src/main/java/com/treinamento/corridaSquad/controller/EquipeController.java
+++ src/main/java/com/treinamento/corridaSquad/controller/EquipeController.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.treinamento.corridaSquad.Mensagem;
+import com.treinamento.corridaSquad.biz.EquipeBiz;
 import com.treinamento.corridaSquad.entities.Equipe;
 import com.treinamento.corridaSquad.repositories.EquipeRepository;
 
@@ -25,4 +29,39 @@ public class EquipeController {
 	public List<Equipe> listarEquipe(){
 		return equipeRepository.findAll();
 	}	
+	
+	@PostMapping("incluir")
+	public Mensagem incluirEquipe(@Validated @RequestBody Equipe equipe) {
+		EquipeBiz equipeBiz = new EquipeBiz();
+		try {
+			if (equipeBiz.validar(equipe)) {
+				equipeRepository.save(equipe);
+				equipeRepository.flush();
+				equipeBiz.getMensagem().mensagem.add("Equipe adicionada");
+			}
+		} catch (Exception e) {
+			equipeBiz.getMensagem().mensagem.add("Erro ao Incluir:" + e.getMessage());
+		}
+		return equipeBiz.getMensagem();
+	}
+	
+	@PutMapping(path="alterar/{id}")
+    public Mensagem alterarEquipe(@PathVariable Integer id, @RequestBody @Validated Equipe equipe) {
+        
+    	EquipeBiz equipeBiz = new EquipeBiz();
+    	equipe.setId(id);
+        try{
+            if(equipeBiz.validar(equipe)) {
+            	equipeRepository.save(equipe);
+                equipeRepository.flush();
+                equipeBiz.getMensagem().mensagem.add("Equipe alterado com sucesso");
+            }else {
+            	equipeBiz.getMensagem().mensagem.add("Erro ao alterar");
+            }
+        	
+        }catch(Exception e) {
+            equipeBiz.getMensagem().mensagem.add("Erro ao alterar: " + e.getMessage());
+        }
+        return equipeBiz.getMensagem();
+    }
 }
