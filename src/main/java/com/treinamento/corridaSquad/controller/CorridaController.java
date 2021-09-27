d1d1464d2898d1722986139373b6e4642533e54c
Lucas D. Soares <lsoares@itelinfo.com>
2021-09-27 11:25:58 -0400

    Implementação da Controller e da BIZ da Corrida

---
+++ src/main/java/com/treinamento/corridaSquad/controller/CorridaController.java
@@ -0,0 +1,64 @@
+package com.treinamento.corridaSquad.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.treinamento.corridaSquad.Mensagem;
+import com.treinamento.corridaSquad.biz.CorridaBiz;
+import com.treinamento.corridaSquad.entities.Corrida;
+import com.treinamento.corridaSquad.repositories.CorridaRepository;
+
+@RestController
+@RequestMapping("corrida")
+//@CrossOrigin("http://localhost:4200")
+public class CorridaController {
+	
+	@Autowired
+	private CorridaRepository corridaRepository;
+	
+	@GetMapping("listar")
+	public List<Corrida> listarCorrida(){
+		List<Corrida> lista = corridaRepository.findAll();
+		return lista;
+	}
+	
+	@PostMapping("incluir")
+	public Mensagem incluirCorrida(@Validated @RequestBody Corrida corrida) {
+		
+		CorridaBiz corridaBiz = new CorridaBiz(this.corridaRepository);
+		try {
+			
+			if (corridaBiz.validar(corrida)) {
+				corridaRepository.save(corrida);
+				corridaRepository.flush();
+				corridaBiz.getMsg().getMensagem().add("OK");
+			}
+			
+		} catch (Exception e) {
+			corridaBiz.getMsg().getMensagem().add("Erro ao Incluir:" + e.getMessage());
+		}
+		return corridaBiz.getMsg();
+	}
+		@PutMapping("alterar/{id}")
+	    public String alterarPiloto(@RequestBody @Validated Corrida corrida) {
+	
+	        try {
+	            corridaRepository.save(corrida);
+	            corridaRepository.flush();
+	            return " Alterado com sucesso";
+	        } catch (Exception e) {
+	            return e.getMessage();
+		}
+
+		}
+}
+
+
