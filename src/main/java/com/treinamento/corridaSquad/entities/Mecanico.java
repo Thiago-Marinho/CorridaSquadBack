a0bcb07e62ab78cc6e5f0fcd7f2d605c2707610e
Misael Lima <misaellimadossantos@hotmail.com>
2021-09-27 09:23:18 -0400

    Adicionando a entidade Mecanico

---
+++ src/main/java/com/treinamento/corridaSquad/entities/Mecanico.java
@@ -0,0 +1,43 @@
+package com.treinamento.corridaSquad.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mecanico")
+public class Mecanico {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "id_equipe")
+	private Integer id_equipe;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getId_equipe() {
+		return id_equipe;
+	}
+	public void setId_equipe(Integer id_equipe) {
+		this.id_equipe = id_equipe;
+	}
+	
+	
+}
