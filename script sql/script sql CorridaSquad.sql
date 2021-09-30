da7a2c260ceeceb77ade21020fa41bd6e3dc7a40
Misael Lima <misaellimadossantos@hotmail.com>
2021-09-30 16:18:04 -0400

    Inserindo as views no script de banco de dados

--- script sql/script sql CorridaSquad.sql
+++ script sql/script sql CorridaSquad.sql
@@ -60,3 +60,32 @@ CREATE TABLE auxiliar (
 	id_mecanico int NOT NULL REFERENCES mecanico(id)
 )
 GO
+
+CREATE VIEW view_quantidade_carro_corrida as
+SELECT
+    cor.id, cor.descricao as corrida, COUNT(*) as quant_carro
+FROM
+    carro as car
+INNER JOIN carro_corrida_piloto as ccp on ccp.id_carro = car.id
+INNER JOIN corrida as cor on cor.id = ccp.id_corrida
+GROUP BY cor.id,cor.descricao
+GO
+
+CREATE VIEW View_corrida_equipe as
+SELECT
+    cor.id, e.nome as equipe, car.descricao as carro, cor.descricao as corrida
+FROM
+    equipe as e
+INNER JOIN carro as car on car.id_equipe = e.id
+INNER JOIN carro_corrida_piloto as ccp on ccp.id_carro = car.id
+INNER JOIN corrida as cor on cor.id = ccp.id_corrida
+GO
+
+CREATE VIEW view_mecanico_auxiliar as
+SELECT
+    mec.id, mec.nome, COUNT(*) as quantidade_auxiliares
+FROM
+    auxiliar as aux
+INNER JOIN mecanico as mec on mec.id = aux.id_mecanico
+GROUP BY mec.id, mec.nome
+GO
