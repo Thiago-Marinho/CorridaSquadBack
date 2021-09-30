3b40da0988407e9e6bb3d82a8a072b529058adee
lucasdsks <lucasdrifitim@hotmail.com>
2021-09-30 14:41:42 -0300

    Corrida Teste

---
+++ src/test/java/com/treinamento/corridaSquad/CorridaTest.java
@@ -0,0 +1,69 @@
+package com.treinamento.corridaSquad;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.treinamento.corridaSquad.controller.CorridaController;
+import com.treinamento.corridaSquad.entities.Corrida;
+import com.treinamento.corridaSquad.repositories.CorridaRepository;
+
+@SpringBootTest
+public class CorridaTest {
+	
+	 @Autowired
+	 CorridaController corridaController;
+	 @Autowired
+	 CorridaRepository corridaRepository;
+	 
+	 @Test
+	 public void carroListarTest(){
+		 Integer expected = 0;
+		 Integer result = 0;
+
+		 expected = (int)corridaRepository.count();
+		 result = corridaController.listarCorrida().size();
+		 assertThat(expected).isEqualTo(result);
+	    }
+	 
+	 @Test 
+	public void corridaIncluirAdd() {
+		Integer expected = (int) corridaRepository.count()+1;
+		Corrida novo = new Corrida();
+		novo.setDescricao("Maclaren");
+		corridaRepository.save(novo);
+		corridaRepository.flush();
+		Integer result = (int) corridaRepository.count();
+		assertThat(expected).isEqualTo(result);
+		}
+	 
+    @Test
+    public void corridaConsultarTest() {
+        List<Corrida> ListCorrida = this.corridaRepository.findAll();
+        Corrida expectedObject = ListCorrida.get(1);
+        Corrida resultObject = this.corridaController.consultar(expectedObject.getId());
+        assertThat(expectedObject.getId()).isEqualTo(resultObject.getId());
+        assertThat(expectedObject.getDescricao()).isEqualTo(resultObject.getDescricao());
+
+	     }
+    
+    @Test
+    public void corridaAlterarTest(){
+        Corrida corrida = new Corrida();
+        corrida.setId(1);
+        corrida.setDescricao("Teste");
+
+        Integer savedCorridaId = corridaRepository.save(corrida).getId();
+
+        corrida.setDescricao("Teste Realizado");
+
+        corridaController.alterarPiloto(corrida);
+        Corrida result = corridaController.consultar(corrida.getId());
+        assertThat(result.getDescricao()).isEqualTo(corrida.getDescricao());
+    }
+	 	
+}
